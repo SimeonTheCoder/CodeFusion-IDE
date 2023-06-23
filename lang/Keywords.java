01f03afe68a7284dd210487de1971dc5501ae0d3
Simeon Petkov <petkov.moni@gmail.com>
2023-06-23 10:10:39 +0300

    Small little baby update

--- lang/Keywords.java
+++ lang/Keywords.java
@@ -12,7 +12,8 @@ public class Keywords {
                     "con",
                     "jmp",
                     "sample",
-                    "out"
+                    "out",
+                    "return"
             )
     );
 
